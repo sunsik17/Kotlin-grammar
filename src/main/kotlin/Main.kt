6a123d274ae7e54eb71a308910177f42e744b489
sunsik <sunsig21c@gmail.com>
2023-08-29 05:36:39 +0900

    val 은 value, java의 final과 비슷 한번 정의 후 바꿀 수 없음 var 은 variable, 재정의 가능 문자열안에 $ 사용하여 수식이나 변수 사용가능 StringTemplate 이라고 함

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,13 @@
 fun main() {
     println("Hello World")
     val value = max(4, 5)
+
+    var age = 29
+    println("Hello, ${if (age > 28) "M" else "B"}")
+
+    age = 27
+    println("Hello, ${if (age > 28) "M" else "B"}")
+
     println("Max value is : $value")
     println(max(1, 2))
     println(max2(2, 3))
