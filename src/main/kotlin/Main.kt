1f5afcd4c601c72ba9cd32006672bad41d2343cf
sunsik <sunsig21c@gmail.com>
2023-08-29 05:24:51 +0900

    kotlin 에서는 java와 다르게 클래스 없이 함수를 만들 수 있다. 또한 코드가 굉장히 간결하다고 느껴지고 반환값에 if else 사용 가능 반환값 명시 하지 않아도 되고 ';' 사용하지 않아도 됌

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,11 @@
 fun main() {
     println("Hello World")
+    println(max(1, 2))
+    println(max2(2, 3))
+    println(max3(2, 4))
 }
+fun max(a: Int, b: Int): Int {
+    return if (a > b) a else b
+}
+fun max2 (a: Int, b: Int) : Int = if (a > b) a else b
+fun max3 (a: Int, b: Int) = if (a > b) a else b
