587c1c1888c6dccacdf58e009f61307041b211a7
sunsik <sunsig21c@gmail.com>
2023-09-05 08:26:53 +0900

    코틀린의 확장함수
    infix fun (중위함수)
    주로 확장함수이면서 파라미터가 한개인 함수에 활용 가능
    가독성 향상과 코드 길이 축소
    주로 mapOf("key" to "value", "k2" to "v2")에서 많이 활용

--- src/main/kotlin/Extension.kt
+++ src/main/kotlin/Extension.kt
@@ -1,6 +1,10 @@
 fun String.double(add: String) = this + this + add
 fun String.toInt() = Integer.parseInt(this)
+infix fun String.add(postfix: String) = this + postfix
+infix fun String.equalTo(expected: String) = this == expected
 fun main() {
     println("do".double(" is best"))
     println("1".toInt())
+    println("do" add " while")
+    println("do" equalTo "do")
 }
