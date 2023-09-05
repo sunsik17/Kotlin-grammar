d44b21b7c964990dc07c3f88e75ba1e0197ecc96
sunsik <sunsig21c@gmail.com>
2023-09-05 09:33:43 +0900

    코틀린의 생성자 -2
    생성자의 프로퍼티 옆에 기본 값을 적어주면 해당 값이 기본 값이 되어 생략 가능
    주 생성자와 기본값이 있는 프로퍼티를 제외한 생성자가 같이 만들어진다는 뜻

--- src/main/kotlin/ConstructUser.kt
+++ src/main/kotlin/ConstructUser.kt
@@ -1,6 +1,7 @@
 // 생성자 및 초기화를 가장 명시적으로 풀어쓴 버전
 class User constructor(_userName: String) {
     val userName: String
+
     init {
         userName = _userName
     }
@@ -13,3 +14,13 @@ class User2 constructor(_userName: String) {
 
 // 실제로 가장 많이 사용되는 버전
 class User3(val userName: String)
+
+data class User4(
+    val userName: String,
+    val level: Int = 1
+)
+
+fun main() {
+    println(User4("Jelly"))
+    println(User4("Hyde", 10))
+}
