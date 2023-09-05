8a52419a9d8c71e1416c35dfa7c5ef085ad05015
sunsik <sunsig21c@gmail.com>
2023-09-05 09:30:23 +0900

    코틀린의 생성자 -1
    constructor 키워드 생략
    init 블록 대신 프로퍼티 바로 초기화
    
    생성자 파라미터에 val 또는 var가 붙으면
    -> 해당 파라미터와 동일한 이름으로 프로퍼티를 생성해준다는 뜻
    -> 생성자 파라미터에 val(var) 이 없는 것과 있는 것을 혼합하는 것도 가능

---
+++ src/main/kotlin/ConstructUser.kt
@@ -0,0 +1,15 @@
+// 생성자 및 초기화를 가장 명시적으로 풀어쓴 버전
+class User constructor(_userName: String) {
+    val userName: String
+    init {
+        userName = _userName
+    }
+}
+
+// 조금 간소화 한 버전
+class User2 constructor(_userName: String) {
+    val userName = _userName
+}
+
+// 실제로 가장 많이 사용되는 버전
+class User3(val userName: String)
