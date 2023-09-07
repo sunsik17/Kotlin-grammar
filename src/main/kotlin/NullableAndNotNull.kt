073ef8517814e0663c2c5c3d18d529ab9ecaf95e
sunsik <sunsig21c@gmail.com>
2023-09-07 09:19:57 +0900

    코틀린의 타입
    nullable 한 클래스 인스턴스에서의 3가지 호출 방법
    - if 조건식으로 타입을 일시적으로 변경 가능
    - ?. 호출로 호출하거나 혹은 null을 주거나
    - !!. 호출로 강제하거나 NPE를 발생하거나

---
+++ src/main/kotlin/NullableAndNotNull.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val nullablePerson0: Person0? =
+        if (System.currentTimeMillis() % 2 == 0L) Person0("Even", 22)
+        else null
+
+
+    // if 검사 등으로 특정 역역에서는 not null 타입처럼 사용 가능
+    if (nullablePerson0 != null) {
+        println("짝수 시간에만 존재하는 사람의 나이는 ${nullablePerson0.age}")
+    }
+
+    // ?. 호출을 수행 : 값이 있는 경우는 호출결과, 값이 없는 경우에는 null
+    println("짝수 시간에만 존재하는 사람의 나이는 ${nullablePerson0?.age}")
+    // !!. 호출을 수행 : 값이 없는 경우 NPE 발생
+    println("짝수 시간에만 존재하는 사람의 나이는 ${nullablePerson0!!.age}")
+}
