edc400c0f26ae966a3e1725d6adfd8ab151ea2f9
sunsik <sunsig21c@gmail.com>
2023-09-07 09:45:27 +0900

    코틀린의 Nothing
    이함수는 정상적으로 끝나는 케이스가 없음(Nothing)

--- src/main/kotlin/Type.kt
+++ src/main/kotlin/Type.kt
@@ -4,8 +4,22 @@ fun main() {
     val l: Long = i.toLong()
 
     printSomething()
+    val persons = listOf(
+        Person0("Captain", 44),
+        Person0("Cyclops", 35),
+        Person0("Deadpool", 31),
+        Person0("Iron Man", 31),
+        Person0("Iceman", 54),
+        Person0("Loki", 54),
+        Person0("Hulk", 54),
+    )
+    val name = persons.find { it.age > 55 } ?: fail("50살이 넘는 사람은 없다")
 }
 
 fun printSomething(): Unit {
     println("Void Method")
+}
+
+fun fail(message: String): Nothing {
+    throw IllegalStateException(message)
 }
